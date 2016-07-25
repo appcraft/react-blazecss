38e21fc75a137a565d79803e8acb079948883807
Gregory Potdevin <gregory.potdevin@gmail.com>
2016-07-25 13:25:06 +0200

    nav demo with playground

--- demo/src/NavDemo.js
+++ demo/src/NavDemo.js
@@ -1,39 +1,46 @@
 import React from 'react'
-import {Nav, NavContent, NavItem} from '../../src'
 import {H2, H3} from '../../src'
+
+import ReactPlayground from './LiveEditor'
 
 export class NavDemo extends React.Component {
   render(){
     return (
       <div id="navs">
         <H2 size="large">Nav</H2>
-        <div>
-          <H3 size="medium">Normal</H3>
-          <Nav style={{width: 200}}>
-            <NavContent centered>My APP</NavContent>
-            <NavItem bStyle="success" active>Home</NavItem>
-            <NavItem>Menu</NavItem>
-            <NavItem bStyle="primary">Settings</NavItem>
-          </Nav>
-        </div>
-        <div>
-          <H3 size="medium">Light</H3>
-          <Nav style={{width: 200}} light>
-            <NavContent centered>My APP</NavContent>
-            <NavItem bStyle="success" active>Home</NavItem>
-            <NavItem>Menu</NavItem>
-            <NavItem bStyle="primary">Settings</NavItem>
-          </Nav>
-        </div>
-        <div>
-          <H3 size="medium">Nav inline</H3>
-          <Nav inline shadow="higher" animate>
-          {/*<Nav inline shadow="higher" animate position="top" fixed>*/}
-            <NavItem bStyle="success" active>Home</NavItem>
-            <NavItem>Menu</NavItem>
-            <NavItem right bStyle="primary">Settings</NavItem>
-          </Nav>
-        </div>
+        <H3 size="medium">Normal</H3>        
+        <ReactPlayground codeText={`\
+export default (\n\
+  <Nav style={{width: 200}}>\n\
+    <NavContent centered>My APP</NavContent>\n\
+    <NavItem bStyle="success" active>Home</NavItem>\n\
+    <NavItem>Menu</NavItem>\n\
+    <NavItem bStyle="primary">Settings</NavItem>\n\
+  </Nav>\n\
+)\
+`} />
+        <H3 size="medium">Light</H3>
+        <ReactPlayground codeText={`\
+export default (\n\
+  <Nav style={{width: 200}} light>\n\
+    <NavContent centered>My APP</NavContent>\n\
+    <NavItem bStyle="success" active>Home</NavItem>\n\
+    <NavItem>Menu</NavItem>\n\
+    <NavItem bStyle="primary">Settings</NavItem>\n\
+  </Nav>\n\
+)\
+`} />
+        <H3 size="medium">Nav inline</H3>
+        <ReactPlayground codeText={`\
+export default (\n\
+  <Nav inline shadow="higher" animate>\n\
+  {/*<Nav inline shadow="higher" animate position="top" fixed>*/}\n\
+    <NavItem bStyle="success" active>Home</NavItem>\n\
+    <NavItem>Menu</NavItem>\n\
+    <NavItem right bStyle="primary">Settings</NavItem>\n\
+  </Nav>\n\
+)\
+`} />
       </div>
     )
   }
