08412cec3dc5f90a8b5672ae566d6b66dbe785a7
Gregory Potdevin <gregory.potdevin@gmail.com>
2016-07-27 14:30:14 +0200

    coverage test

--- tests/index-test.js
+++ tests/index-test.js
@@ -2,6 +2,8 @@
 import expect from 'expect'
 import expectJSX from 'expect-jsx';
 
+require('../src') // Import everything to fix coverage
+
 expect.extend(expectJSX);
 
 require('./Button.unit.js')
