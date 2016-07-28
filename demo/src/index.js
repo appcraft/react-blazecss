c3da2dcbed0ecbd700cb2c0cbb831fe9c6fe4ddf
Gregory Potdevin <gregory.potdevin@gmail.com>
2016-07-28 23:58:18 +0200

    GA test (again)

--- demo/src/index.js
+++ demo/src/index.js
@@ -35,14 +35,22 @@ require('blaze/dist/blaze.animations.min.css')
 require('./css/demo.css')
 
 class GA extends React.Component {
-  componentDidMount() {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  componentWillMount() {
+    const { id } = this.props
+    const script = document.createElement("script");
 
-    ga('create', this.props.id, 'auto');
-    ga('send', 'pageview');
+    script.innerText = `
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', '${id}', 'auto');
+  ga('send', 'pageview');
+    `;
+    console.log(script)
+
+    document.body.appendChild(script);
   }
 
   render(){
